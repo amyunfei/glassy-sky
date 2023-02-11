cf056bb455d2ce5d16932baed59b0f55d222b46b
郝云飞 <haoyf@botech.com.cn>
2023-02-11 22:22:53 +0800

    update

--- internal/admin/dto/category_dto.go
+++ internal/admin/dto/category_dto.go
@@ -1,11 +1,9 @@
 package dto
 
 import (
-	"reflect"
 	"strconv"
 
 	"github.com/amyunfei/glassy-sky/internal/admin/domain/postgresql"
-	"github.com/go-playground/validator"
 )
 
 type CreateCategoryRequest struct {
@@ -30,25 +28,4 @@ func (c *CreateCategoryResponse) Transform(modal postgresql.Category) {
 	c.Color = strconv.FormatInt(int64(modal.Color), 16)
 	c.CreatedAt = modal.CreatedAt.Format("2006-01-02 15:04:05")
 	c.UpdatedAt = modal.UpdatedAt.Format("2006-01-02 15:04:05")
-}
-
-// 自定义错误消息
-func GetError(errs validator.ValidationErrors, r interface{}) string {
-	s := reflect.TypeOf(r)
-	for _, fieldError := range errs {
-		filed, _ := s.FieldByName(fieldError.Field())
-		errTag := fieldError.Tag() + "_err"
-		// 获取对应binding得错误消息
-		errTagText := filed.Tag.Get(errTag)
-		// 获取统一错误消息
-		errText := filed.Tag.Get("err")
-		if errTagText != "" {
-			return errTagText
-		}
-		if errText != "" {
-			return errText
-		}
-		return fieldError.Field() + ":" + fieldError.Tag()
-	}
-	return ""
 }
