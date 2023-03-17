e4d1a0a6058dc6f1c8c15b23d9294f4123101b35
郝云飞 <haoyf@botech.com.cn>
2023-03-17 21:20:15 +0800

    add jwt token unit test

--- internal/admin/infrastructure/token/jwt_marker_test.go
+++ internal/admin/infrastructure/token/jwt_marker_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/amyunfei/glassy-sky/internal/admin/infrastructure/utils"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +29,38 @@ func TestJWTMaker(t *testing.T) {
 	require.Equal(t, username, payload.Username)
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	secretKey := utils.RandomString(30)
+	maker, err := NewJWTMaker(secretKey)
+	require.NoError(t, err)
+
+	username := utils.RandomString(6)
+	token, err := maker.CreateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(utils.RandomString(5), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	secretKey := utils.RandomString(30)
+	maker, err := NewJWTMaker(secretKey)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
 }
